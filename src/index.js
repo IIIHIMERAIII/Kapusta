b97cd9458612c199780d35f4a219bc3c8af27e1c
Iuliia Sokol <avare779@gmail.com>
2023-01-17 21:56:23 +0200

    validation hints added to forms

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/kapusta">
+        <BrowserRouter basename="/Kapusta">
           <ThemeProvider theme={theme}>
             <App />
           </ThemeProvider>
