6dee8fa43935d0caa19c208b7eaadf193e0ebc54
Maksym <PTNPNH722@gmail.com>
2023-01-19 11:34:33 +0200

    some fix Balance

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -6,8 +6,7 @@ export function Balance() {
   return (
     <BalanceContainer>
       <BalanceFrom />
-
-      <StyledLink to="">
+      <StyledLink to='/statistics'>
         Reports
         <ReportsSvg>
           <use href={`${svg}#reports`}></use>
--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,14 +1,12 @@
 import { Balance } from 'components/Balance/Balance';
 import InputTransactionForm from 'components/InputTransactionForm/InputTransactionForm';
-import { NavLink } from 'react-router-dom';
 
 const WalletPage = () => {
   return (
     <>
       <h1>Wallet</h1>
       <Balance />;
-      <NavLink to='/statistics'>Statistics</NavLink>
-      <InputTransactionForm type="expence" />
+      <InputTransactionForm type="expense" />
     </>
   );
 };
