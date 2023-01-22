12f0280b2dffa82de8065fca1d960e7b8d3872e5
Maksym <PTNPNH722@gmail.com>
2023-01-22 10:34:52 +0200

    media-query

--- src/components/WalletTabs/WalletTabs.jsx
+++ src/components/WalletTabs/WalletTabs.jsx
@@ -13,13 +13,10 @@ import Summary from 'components/Summary/Summary';
 
 export const Tabs = () => {
   const [activeTab, setActiveTab] = useState('tab1');
-  //  Functions to handle Tab Switching
   const handleTab1 = () => {
-    // update the state to tab1
     setActiveTab('tab1');
   };
   const handleTab2 = () => {
-    // update the state to tab2
     setActiveTab('tab2');
   };
   return (
