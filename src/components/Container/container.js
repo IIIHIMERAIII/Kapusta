eb735d81286a3fa1704cd03ae23063de8a55fd52
Maksym <PTNPNH722@gmail.com>
2023-01-22 10:54:36 +0200

    media rules Main Container

--- src/components/Container/container.js
+++ src/components/Container/container.js
@@ -2,9 +2,15 @@ import styled from "styled-components";
 import wallet_bg from '../../images/wallet_bg.png'
 
 export const Main = styled.main`
-width: 1100px;
+width: 280px;
 margin-left: auto;
 margin-right: auto;
+@media screen and (min-width: 768px) {
+width: 704px; 
+} 
+@media screen and (min-width: 1280px){
+width: 1100px;
+}
 `;
 
 export const Bg = styled.div`
@@ -12,4 +18,5 @@ export const Bg = styled.div`
   background-repeat: no-repeat;
   background-size: cover;
   height: 100vh;
-`
+`;
+
