994b8603f5df3f946ec411a7527a5d67a9232109
Dan Chuzhov <dan.chuzhov.htc@gmail.com>
2023-01-23 10:20:58 +0200

    ITF responsive

--- src/components/InputTransactionForm/styles/InputTransactionForm.styled.js
+++ src/components/InputTransactionForm/styles/InputTransactionForm.styled.js
@@ -16,14 +16,21 @@ export const MainContainer = styled.div`
   @media screen and (min-width: 768px) and (max-width: 1279.5px) {
     width: 624px;
 
+    margin-bottom: 48px;
+
     background-color: white;
     border-bottom-left-radius: 0;
   }
 
   @media screen and (min-width: 1280px) {
     flex-wrap: nowrap;
+    min-width: 1034px;
+    height: 44px;
 
     margin-bottom: 60px;
+
+    background-color: #ffffff;
+    border-bottom-left-radius: 0;
   }
 `;
 
@@ -39,17 +46,22 @@ export const InputForm = styled.form`
 
   @media screen and (min-width: 768px) and (max-width: 1279.5px) {
     width: 624px;
+    min-width: 624px;
     height: 44px;
 
     row-gap: 34px;
     column-gap: 34px;
-
-    margin-bottom: 48px;
   }
 
   @media screen and (min-width: 1280px) {
     display: inline-flex;
+    flex-wrap: nowrap;
     column-gap: 32px;
+
+    min-width: 1034px;
+    width: 1034px;
+
+    margin-bottom: 0;
   }
 `;
 
@@ -58,18 +70,25 @@ export const InputGroupWrapper = styled.div`
   width: 100%;
 
   @media screen and (min-width: 768px) and (max-width: 1279.5px) {
-    display: inline-flex;
-    position: relative;
+    display: flex;
+    flex-wrap: nowrap;
 
     height: 44px;
     width: 478px;
 
     border: 2px solid #f6f7fc;
     border-radius: 16px 16px 16px 0;
+  }
 
-    @media screen and (min-width: 1280px) {
-      width: 578px;
-    }
+  @media screen and (min-width: 1280px) {
+    display: flex;
+    flex-wrap: nowrap;
+
+    width: 578px;
+    height: 44px;
+
+    border: 2px solid #f6f7fc;
+    border-radius: 16px 16px 16px 0;
   }
 `;
 
@@ -77,7 +96,6 @@ export const Input = styled.input`
   box-sizing: border-box;
 
   height: 44px;
-  width: 100%;
 
   background-color: #f5f6fb;
   border: 2px solid #ffffff;
@@ -86,18 +104,25 @@ export const Input = styled.input`
   font-size: 12px;
   outline: none;
 
+  ::placeholder {
+    font-family: inherit;
+    font-size: 12px;
+    font-weight: 400;
+    color: #c7ccdc;
+  }
+
   @media screen and (min-width: 768px) and (max-width: 1279.5px) {
     height: 40px;
     border: none;
 
     background-color: #ffffff;
+  }
 
-    ::placeholder {
-      font-family: inherit;
-      font-size: 12px;
-      font-weight: 400;
-      color: #c7ccdc;
-    }
+  @media screen and (min-width: 1280px) {
+    height: 40px;
+    border: none;
+
+    background-color: #ffffff;
   }
 `;
 
@@ -110,16 +135,22 @@ export const InputProduct = styled(Input)`
   border-top-right-radius: 16px;
 
   @media screen and (min-width: 768px) and (max-width: 1279.5px) {
+    width: 184px;
     border-top-left-radius: 0px;
     border-top-right-radius: 16px;
   }
   @media screen and (min-width: 1280px) {
     width: 290px;
+
+    border-top-left-radius: 0px;
+    border-top-right-radius: 16px;
   }
 `;
 
 export const InputAmountWrapper = styled.div`
-  @media screen and (max-width: 767.9px) {
+  position: relative;
+
+  /* @media screen and (max-width: 767.9px) {
     display: flex;
     position: relative;
 
@@ -130,15 +161,20 @@ export const InputAmountWrapper = styled.div`
 
     width: 183px;
   }
+  @media screen and (min-width: 768px) and (max-width: 1279.5px) {
+    display: flex;
+    position: relative;
+  } */
 `;
 
 export const InputAmount = styled(Input)`
+  text-align: right;
+
   @media screen and (min-width: 768px) and (max-width: 1279.5px) {
     flex-shrink: 0;
     padding-right: 42px;
     width: 110px;
 
-    text-align: right;
     border-radius: 0 16px 16px 0;
     overflow: hidden;
   }
